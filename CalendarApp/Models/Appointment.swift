e6a43a85e9a3a7afff8ee8ec306d24ad50f70c1e
tinchovictory <martin@victory.com.ar>
2020-02-08 01:01:16 -0300

    adding models

---
+++ CalendarApp/Models/Appointment.swift
@@ -0,0 +1,17 @@
+//
+//  Appointment.swift
+//  CalendarApp
+//
+//  Created by Martin Victory on 08/02/2020.
+//  Copyright © 2020 Martin Victory. All rights reserved.
+//
+
+import Foundation
+
+struct Appointment {
+    let id: Int
+    let title: String
+    let date: Date
+    let startTime: Int // change it to a time struct
+    let endTime: Int // change it to a time struct
+}
---
+++ CalendarApp/Models/AppointmentGroup.swift
@@ -0,0 +1,15 @@
+//
+//  AppointmentGroup.swift
+//  CalendarApp
+//
+//  Created by Martin Victory on 08/02/2020.
+//  Copyright © 2020 Martin Victory. All rights reserved.
+//
+
+import Foundation
+
+struct AppointmentGroup {
+    let id: Int
+    let name: String
+    let color: RGBValue
+}
