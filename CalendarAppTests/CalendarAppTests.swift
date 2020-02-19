b31f44be660b5c438ef6ebcfe73e73ee4b97b563
tinchovictory <martin@victory.com.ar>
2020-02-19 00:18:43 -0300

    adding test target

---
+++ CalendarAppTests/CalendarAppTests.swift
@@ -0,0 +1,26 @@
+//
+//  CalendarAppTests.swift
+//  CalendarAppTests
+//
+//  Created by Martin Victory on 19/02/2020.
+//  Copyright © 2020 Martin Victory. All rights reserved.
+//
+
+import XCTest
+
+class CalendarAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
